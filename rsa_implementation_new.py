5f2787008691abae3603ed7eac567202ff90d723
vijayvargiyaamogh-crypto <vijayvargiyaamogh@gmail.com>
2026-08-22 20:32:51 +0530

    Update Python script rsa_implementation_new.py
    Had accidentally uploaded an older version of the script; fixed this now.

--- rsa_implementation_new.py
+++ rsa_implementation_new.py
@@ -1,7 +1,13 @@
-p = 13
-q = 67
+p = 67
+q = 71
 n = p * q
-message = input("Enter the text to be encrypted: ")
+phi = (p-1) * (q-1)
+e = 65537
+d = pow(e, -1, phi)
+message = input("Enter the message to be encrypted: ")
 unicode = bytearray(message, "utf-8")
-encrypt = bytes(pow(c, 65537, n) for c in unicode)
-print(f"The encrypted text is: {encrypt.decode('utf-8')}")
+encrypted = [pow(c, e, n) for c in unicode]
+print(f"The encrypted message is: {encrypted}")
+encrypted = [int(c) for c in input("Enter the message to be decrypted: ").split()]
+decrypted = bytes(pow(c, d, n) for c in encrypted)
+print(f"The decrypted message is: {decrypted.decode('utf-8')}")
